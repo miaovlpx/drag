d7fc377ba949c909412c6bbb504327becd7653fd
ghostdp <new666@qq.com>
2015-05-19 12:01:04 +0800

    change drag.js from new1

--- drag.js
+++ drag.js
@@ -7,5 +7,9 @@ function drag(id){
 	obj.onmousedown = function(ev){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev){
+			obj.style.left = ev.clientX - disX + 'px';
+			obj.style.top = ev.clientY - disX + 'px';
+		};
 	}
 }
